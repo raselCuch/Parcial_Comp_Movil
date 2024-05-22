faa7c6680381965fc392e18d62d4085eee412ff4
Rasel <ruselcucho@gmail.com>
2024-05-22 00:00:45 -0500

    commit 2

--- app/src/main/java/com/example/practicando_parcial/formulario.java
+++ app/src/main/java/com/example/practicando_parcial/formulario.java
@@ -1,9 +1,6 @@
 package com.example.practicando_parcial;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    public EditText etContrasenha, etUsuario;
+public class formulario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_formulario);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        etContrasenha = findViewById(R.id.txtContrasenha);
-        etUsuario = findViewById(R.id.txtUsuario);
+
     }
-
-    public void ingresar(View view){
-        Intent i = new Intent(this, formulario.class);
-        i.putExtra("usuario", etUsuario.getText().toString());  // envio de datos
-        i.putExtra("contrasenha", etContrasenha.getText().toString());  // envio de datos
-
-        startActivity(i);
-        finish();
-    }
-
 }
